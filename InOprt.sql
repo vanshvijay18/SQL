10b6d2f1807f8dce6d1db5e1e969b34e304ae057
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-02-26 21:34:27 +0530

    Create InOprt.sql

---
+++ InOprt.sql
@@ -0,0 +1,3 @@
+SELECT * FROM country WHERE continent ='North America';
+SELECT * FROM country WHERE continent in ('North America','Europe');
+SELECT * FROM country WHERE continent in ('North America','Europe','ASIA');
