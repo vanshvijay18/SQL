8a59e1678e192cdf6e334e93dc0e844eac11f4e6
Vansh Vijayvargiya <146448479+vanshvijay18@users.noreply.github.com>
2026-02-26 21:37:53 +0530

    Create Distunctt.sql

---
+++ Distunctt.sql
@@ -0,0 +1,5 @@
+-- DISTINCT() --> only receive distinct value (find to unique value from one or more column we use distinct)
+ SELECT distinct (continent) FROM country;
+ SELECT distinct continent, region FROM country;
+ 
+ -- SUM() , AVG()
